76397848c4f92fbd6a7f242570b93541098d05df
Ryan Kwon <kznm.develop@gmail.com>
2021-12-19 13:29:15 +0900

    Create 6359-algorithm_SOLVING.cpp

---
+++ baekjoon_cpp/6359-algorithm_SOLVING.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, N;
+    bool room[100] = {false,};
+    scanf("%d", &T);
+    for(int i=0; i<T; i++) {
+        
+    }
+}
