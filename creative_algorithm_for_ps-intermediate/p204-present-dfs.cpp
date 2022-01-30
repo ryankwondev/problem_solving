28903716e17f4378b88d7cda08e768b6fa89dda6
Ryan Kwon <kznm.develop@gmail.com>
2022-01-30 13:40:15 +0900

    Create p204-present-dfs.cpp

---
+++ creative_algorithm_for_ps-intermediate/p204-present-dfs.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, arr[15], diff = 987654321, A, B, C;
+
+void func(int level, int a, int b, int c) {
+    if (level >= N)
+        if (a < b || b < c) {
+            return;
+        } else {
+            if (a - c < diff) {
+                diff = a - c;
+                A = a;
+                B = b;
+                C = c;
+            } else if (a - c == diff && A > a) {
+                A = a;
+                B = b;
+                C = c;
+            }
+            return;
+        }
+
+    func(level + 1, a + arr[level], b, c);
+    func(level + 1, a, b + arr[level], c);
+    func(level + 1, a, b, c + arr[level]);
+}
+
+int main() {
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+
+    func(0, 0, 0, 0);
+    printf("%d %d %d", A, B, C);
+}
