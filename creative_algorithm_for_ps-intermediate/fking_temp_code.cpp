61d706b93e1df45a62005488272207bc83bc4085
Ryan Kwon <kznm.develop@gmail.com>
2022-01-30 18:27:23 +0900

    Create fking_temp_code.cpp

---
+++ creative_algorithm_for_ps-intermediate/fking_temp_code.cpp
@@ -0,0 +1,25 @@
+/* p.238 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char durumary[10];
+char fking_dari_1[20];
+char fking_dari_2[20];
+int cnt = 0;
+
+void func(int duru_idx, int fkdr_idx, int where_the_fk_dari_updown) {
+
+}
+
+int main() {
+    scanf("%s", durumary);
+    scanf("%s", fking_dari_1);
+    scanf("%s", fking_dari_2);
+
+    func(0, 0, 0);
+    func(0, 0, 1);
+
+    printf("%d", &cnt);
+}
