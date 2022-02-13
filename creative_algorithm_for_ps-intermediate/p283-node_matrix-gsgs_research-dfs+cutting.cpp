d2599d58109908d7ac152268f2ea161132ecf40b
Ryan Kwon <kznm.develop@gmail.com>
2022-02-13 14:01:59 +0900

    Create p283-node_matrix-gsgs_research-dfs+cutting.cpp

---
+++ creative_algorithm_for_ps-intermediate/p283-node_matrix-gsgs_research-dfs+cutting.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, mini = 987654321;
+int arr[11][11], visited[11];
+
+void func(int node, int s) {
+    if (node == N) {
+        mini = min(mini, s);
+        return;
+    }
+
+    // Cutting
+    if (s > mini)
+        return;
+
+    for (int i = 1; i <= N; i++) {
+        if (arr[node][i] && !visited[i]) {
+            visited[i] = 1;
+            func(i, s + arr[node][i]);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+
+    for (int i = 0; i < M; i++) {
+        int tmpA, tmpB, tmpC;
+        scanf("%d %d %d", &tmpA, &tmpB, &tmpC);
+        if (tmpA == tmpB) continue;
+        arr[tmpA][tmpB] = arr[tmpB][tmpA] = tmpC;
+    }
+
+    visited[1] = 1;
+    func(1, 0);
+    visited[1] = 0;
+
+    printf("%d", mini);
+    return 0;
+}
