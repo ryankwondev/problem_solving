4ac304afff0f00f55fb3aee16d523ce2a2899e12
Ryan Kwon <kznm.develop@gmail.com>
2022-07-03 14:28:20 +0900

    Create 최장증가부분수열.cpp

---
+++ pattern/최장증가부분수열.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, idx, LIS[1000000], arr[1000000];
+
+
+int main() {
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] > LIS[idx]) {
+            LIS[++idx] = arr[i];
+        } else {
+            int t = upper_bound(LIS + 1, LIS + 1 + idx, arr[i]) - LIS;
+            LIS[t] = arr[i];
+        }
+//        printf("%d: %d\n", i, LIS[i]);
+    }
+
+    printf("%d", idx);
+    return 0;
+}
